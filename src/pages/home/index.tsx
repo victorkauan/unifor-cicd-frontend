8e0f7b5b183c3e3a95eaed9be69c6ae628ad40b1
Victor Kauan <victorkauan.dev@gmail.com>
2025-08-23 10:08:04 -0300

    :sparkles: feat: Add header to home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -13,6 +13,7 @@ import findAllProductsService from "../../services/find-all-products.service";
 import findAllMovementsService from "../../services/find-all-movements.service";
 import type { TProduct } from "../../@types/product";
 import type { TMovement } from "../../@types/movement";
+import Header from "../../components/header";
 
 export default function Home() {
   const [products, setProducts] = useState<TProduct[]>([]);
@@ -34,63 +35,68 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="max-w-7xl mx-auto p-4 flex flex-col gap-4">
-      <section className="flex flex-col gap-2">
-        <Subheading>Histórico de movimentações</Subheading>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableHeader>Produto</TableHeader>
-              <TableHeader>Tipo</TableHeader>
-              <TableHeader>Valor unitário</TableHeader>
-              <TableHeader>Quantidade</TableHeader>
-              <TableHeader>Data</TableHeader>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {movements.map((movement) => (
+    <>
+      <Header />
+      <main className="max-w-7xl mx-auto p-4 flex flex-col gap-4">
+        <section className="flex flex-col gap-2">
+          <Subheading>Histórico de movimentações</Subheading>
+          <Table>
+            <TableHead>
               <TableRow>
-                <TableHeader>{movement.nomeProduto}</TableHeader>
-                <TableData>{movement.tipo}</TableData>
-                <TableData>{formatCurrency(movement.valorUnitario)}</TableData>
-                <TableData>{movement.quantidade}</TableData>
-                <TableData>{formatDateTime(movement.dataHora)}</TableData>
+                <TableHeader>Produto</TableHeader>
+                <TableHeader>Tipo</TableHeader>
+                <TableHeader>Valor unitário</TableHeader>
+                <TableHeader>Quantidade</TableHeader>
+                <TableHeader>Data</TableHeader>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </section>
-      <section className="flex flex-col gap-2">
-        <Subheading>Produtos</Subheading>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableHeader>Nome</TableHeader>
-              <TableHeader>Preço</TableHeader>
-              <TableHeader>Quantidade</TableHeader>
-              <TableHeader>Estoque baixo</TableHeader>
-              <TableHeader>Data de entrada</TableHeader>
-              <TableHeader>Última saída</TableHeader>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {products.map((product) => (
+            </TableHead>
+            <TableBody>
+              {movements.map((movement) => (
+                <TableRow>
+                  <TableHeader>{movement.nomeProduto}</TableHeader>
+                  <TableData>{movement.tipo}</TableData>
+                  <TableData>
+                    {formatCurrency(movement.valorUnitario)}
+                  </TableData>
+                  <TableData>{movement.quantidade}</TableData>
+                  <TableData>{formatDateTime(movement.dataHora)}</TableData>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </section>
+        <section className="flex flex-col gap-2">
+          <Subheading>Produtos</Subheading>
+          <Table>
+            <TableHead>
               <TableRow>
-                <TableHeader>{product.nome}</TableHeader>
-                <TableData>{formatCurrency(product.preco)}</TableData>
-                <TableData>{product.quantidade}</TableData>
-                <TableData>{product.estoqueBaixo ? "SIM" : "NÃO"}</TableData>
-                <TableData>{formatDate(product.dataEntrada)}</TableData>
-                <TableData>
-                  {product.ultimaSaida
-                    ? formatDateTime(product.ultimaSaida)
-                    : "-"}
-                </TableData>
+                <TableHeader>Nome</TableHeader>
+                <TableHeader>Preço</TableHeader>
+                <TableHeader>Quantidade</TableHeader>
+                <TableHeader>Estoque baixo</TableHeader>
+                <TableHeader>Data de entrada</TableHeader>
+                <TableHeader>Última saída</TableHeader>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </section>
-    </main>
+            </TableHead>
+            <TableBody>
+              {products.map((product) => (
+                <TableRow>
+                  <TableHeader>{product.nome}</TableHeader>
+                  <TableData>{formatCurrency(product.preco)}</TableData>
+                  <TableData>{product.quantidade}</TableData>
+                  <TableData>{product.estoqueBaixo ? "SIM" : "NÃO"}</TableData>
+                  <TableData>{formatDate(product.dataEntrada)}</TableData>
+                  <TableData>
+                    {product.ultimaSaida
+                      ? formatDateTime(product.ultimaSaida)
+                      : "-"}
+                  </TableData>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </section>
+      </main>
+    </>
   );
 }
