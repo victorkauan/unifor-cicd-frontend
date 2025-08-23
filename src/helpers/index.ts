88ee681248f67cdf6fef9378b9aa2a17f9895ad3
Victor Kauan <victorkauan.dev@gmail.com>
2025-08-23 09:47:44 -0300

    :sparkles: feat: Create format currency function

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function formatCurrency(value: number): string {
+  return value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+}
+
