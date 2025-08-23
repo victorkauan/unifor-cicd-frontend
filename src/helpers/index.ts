5f3f7bca57e358c963528ef3f80e9c920c56f734
Victor Kauan <victorkauan.dev@gmail.com>
2025-08-23 09:48:12 -0300

    :sparkles: feat: Create format date helpers

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { format, parseISO } from "date-fns";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -12,3 +13,12 @@ export function formatCurrency(value: number): string {
   });
 }
 
+export function formatDateBR(date: Date | string): string {
+  const d = typeof date === "string" ? parseISO(date) : date;
+  return format(d, "dd/MM/yyyy");
+}
+
+export function formatDateTime(date: Date | string): string {
+  const d = typeof date === "string" ? parseISO(date) : date;
+  return format(d, "dd/MM/yyyy HH:mm:ss");
+}
