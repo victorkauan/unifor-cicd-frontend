98a708c5d89b598b335aec55a169fa543b5c22f4
Victor Kauan <victorkauan.dev@gmail.com>
2025-08-23 10:07:57 -0300

    :sparkles: feat: Create header component

---
+++ src/components/header/index.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header>
+      <nav className="text-gray-700 bg-white border-b-1 border-gray-200 shadow px-4 lg:px-6 py-3">
+        <div className="flex justify-center items-center">
+          <span className="self-center text-xl font-semibold whitespace-nowrap">
+            UNIFOR CI/CD
+          </span>
+        </div>
+      </nav>
+    </header>
+  );
+}
