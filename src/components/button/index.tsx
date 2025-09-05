2496354384d6dd7eaa2d47ab415662c5a102305c
Victor Kauan <victorkauan.dev@gmail.com>
2025-09-05 00:20:47 -0300

    feat: Create button component

---
+++ src/components/button/index.tsx
@@ -0,0 +1,21 @@
+import { cn } from "../../helpers";
+
+type TButtonProps = React.ComponentPropsWithoutRef<"button">;
+
+export default function Button({
+  children,
+  className,
+  ...props
+}: TButtonProps) {
+  return (
+    <button
+      {...props}
+      className={cn(
+        "text-white font-semibold bg-neutral-600 px-4 py-2 rounded flex items-center gap-1 transition-colors hover:cursor-pointer hover:bg-neutral-500",
+        className
+      )}
+    >
+      {children}
+    </button>
+  );
+}
