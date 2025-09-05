1332461ab55366d4310ca680391da6d24a1b47ea
Victor Kauan <victorkauan.dev@gmail.com>
2025-09-05 00:43:54 -0300

    style: Adjust header design and fix description

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,10 +1,10 @@
 export default function Header() {
   return (
     <header className="bg-white w-full px-8 py-6 shadow fixed top-0 left-0 z-10">
-      <nav className="text-neutral-900 max-w-7xl mx-auto">
+      <nav className="text-neutral-900 max-w-7xl mx-auto flex justify-center">
         <div>
-          <h1 className="self-center text-3xl font-bold">UNIFOR CI/CD</h1>
-          <p>Sistema de control de estoque</p>
+          <h1 className="self-center text-3xl text-center font-bold">UNIFOR CI/CD</h1>
+          <p>Sistema de controle de estoque</p>
         </div>
       </nav>
     </header>
