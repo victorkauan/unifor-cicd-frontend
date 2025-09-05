1a7c39b1c27a5395ccd014a6495b98d1ae5ee0fd
Victor Kauan <victorkauan.dev@gmail.com>
2025-09-05 00:04:47 -0300

    feat: Create helper to get product status label

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -22,3 +22,16 @@ export function formatDateTime(date: Date | string): string {
   const d = typeof date === "string" ? parseISO(date) : date;
   return format(d, "dd/MM/yyyy HH:mm:ss");
 }
+
+export function getProductStatusLabel(status: string) {
+  switch (status) {
+    case "IN_STOCK":
+      return "Em estoque";
+    case "LOW_STOCK":
+      return "Estoque baixo";
+    case "OUT_OF_STOCK":
+      return "Fora de estoque";
+    default:
+      return "Status desconhecido";
+  }
+}
