67309c51308b1d79048ec9f178783d2abbd28985
Victor Kauan <victorkauan.dev@gmail.com>
2025-09-05 00:09:04 -0300

    style: Fix header position

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="bg-white w-full px-8 py-6 shadow fixed top-0 left-0">
+    <header className="bg-white w-full px-8 py-6 shadow fixed top-0 left-0 z-10">
       <nav className="text-neutral-900 max-w-7xl mx-auto">
         <div>
           <h1 className="self-center text-3xl font-bold">UNIFOR CI/CD</h1>
